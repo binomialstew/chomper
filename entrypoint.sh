836909027856dce30f30940f1e5a5f54427dd25b
Charles Lehnert <charles@cllinteractive.com>
2026-05-20 11:01:46 -0400

    chore: improve log wording

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "$SCHEDULE flock -n /reduce_directory /chomper.sh -d /reduce_directory/ /volume_usage_directory ${THRESHOLD} ${FILE_NUMBER}" > /etc/crontabs/root
 
-echo "${FILE_NUMBER} oldest file will be deleted per loop in 'reduce_directory' until 'volume_usage_directory' reaches ${THRESHOLD}% usage according to the following cron expression: ${SCHEDULE}"
+echo -e "The oldest ${FILE_NUMBER} file(s) will be deleted per loop in 'reduce_directory' until 'volume_usage_directory' \nreaches ${THRESHOLD}% usage according to the following cron expression: ${SCHEDULE}\n"
 ## Always run under tini, since we need to reap the leftovers
 exec /sbin/tini -- "$@"
